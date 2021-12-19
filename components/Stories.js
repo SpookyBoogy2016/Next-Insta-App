eb1fc601097959086a325c1a07345769c2c96280
Tino Liu <justinoliu.work@gmail.com>
2021-12-19 17:22:41 +0800

    Added faker.js to generate random names and user profiles

--- components/Stories.js
+++ components/Stories.js
@@ -1,23 +1,28 @@
-import faker from "faker";
-import React, { useEffect } from "react";
+import faker, { image } from "faker";
+import Story from "./Story";
+import React, { useEffect, useState } from "react";
 
 function Stories() {
+  const [suggestions, setSuggestion] = useState([]);
+
   useEffect(() => {
-    const suggestions = [...Array(20)].map((_, i) => ({
+    const suggestions = [...Array(10)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
       id: i,
     }));
     console.log(suggestions);
+    setSuggestion(suggestions);
   }, []);
 
   return (
-    <div>
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      {/*  */}
-      {/*  */}
+    <div className="flex">
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
     </div>
   );
 }
